5daee3658447792f99595d3efb3b13d2e205fe5e
Ricky Wei <God1W@outlook.com>
2020-06-17 11:28:47 -0400

    vue init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    title: '茴香豆有四种写法呢',
+    description: 'need to learn',
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        base: '/blog/',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Github', link: 'https://github.com/RickyWei' },
+            { text: 'LinkedIn', link: 'https://www.linkedin.com/in/ruiji-wei' }
+        ],
+        sidebar: {
+            sidebarDepth: 2,
+            '/': [
+                {
+                    title: '网络',
+                    path: '/network/',
+                    children: [
+                        '/network/应用层',
+                        '/network/传输层'
+                    ]
+                }
+            ]
+        },
+        lastUpdated: 'Last Updated'
+    }
+}
