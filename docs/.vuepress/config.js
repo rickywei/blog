b622a9f1ce018de006c68e258ca88fe50c3ea493
Ricky <god1w@outlook.com>
2020-10-24 00:56:23 -0400

    theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,16 +9,16 @@ module.exports = {
     themeConfig: {
         nav: [
             { text: 'Home', link: '/' },
-            {
-                text: 'Blog', items: [
-                    { text: 'Algorithm', link: '/pages/algorithm/' },
-                    { text: 'C++', link: '/pages/cpp/' },
-                    { text: 'Network', link: '/pages/network/' },
-                    { text: 'OS', link: '/pages/os/' },
-                    { text: 'Design', link: '/pages/design/' },
-                    { text: 'collection', link: '/pages/collection/' },
-                ]
-            },
+            // {
+            //     text: 'Blog', items: [
+            //         { text: 'Algorithm', link: '/pages/algorithm/' },
+            //         { text: 'C++', link: '/pages/cpp/' },
+            //         { text: 'Network', link: '/pages/network/' },
+            //         { text: 'OS', link: '/pages/os/' },
+            //         { text: 'Design', link: '/pages/design/' },
+            //         { text: 'collection', link: '/pages/collection/' },
+            //     ]
+            // },
             { text: 'Github', link: 'https://github.com/RickyWei' },
             { text: 'LinkedIn', link: 'https://www.linkedin.com/in/ruiji-wei' }
         ],
