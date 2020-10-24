2fe4c478216c86bdb6331632d682b089640d5b51
Ricky <god1w@outlook.com>
2020-10-24 11:54:33 -0400

    theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,6 @@ module.exports = {
             { text: 'Github', link: 'https://github.com/RickyWei' },
             { text: 'LinkedIn', link: 'https://www.linkedin.com/in/ruiji-wei' }
         ],
-        sidebar: 'auto',
         lastUpdated: 'Last Updated'
     },
     plugins: [
