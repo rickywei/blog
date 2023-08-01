04c3566b601b1dafe3479d26543c582749536732
RickyWei <God1W@outlook.com>
2023-08-01 11:14:52 +0800

    docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/ricky/Desktop/code/myblog/node_modules/infima/dist/css/default/default.css'),
+  require('/home/ricky/Desktop/code/myblog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/ricky/Desktop/code/myblog/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/ricky/Desktop/code/myblog/src/css/custom.css'),
+];
---
+++ build/assets/js/bda927ed.2299665c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmyblog=self.webpackChunkmyblog||[]).push([[7926],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
